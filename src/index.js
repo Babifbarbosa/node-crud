3299d5bf567875e164ca96fa44ff90d30f249113
Felipe Braga Alfredo <babiferreirab15@gmail.com>
2022-11-03 15:49:42 +0100

    feat:basic login with authentication

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import userRouter from "./routes/users.routes";
 
 const app = express();
+app.use(express.json());
 app.use("/users", userRouter);
 
-app.listen(3000);
+app.listen(3333);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,12 +4,16 @@ import listUsersController from "../controllers/listUsers.controller";
 import updateUserController from "../controllers/updateUser.controller";
 import deleteUserController from "../controllers/deleteUser.controller";
 import createUserController from "../controllers/createUser.controllers";
+import userLoginController from "../controllers/userLogin.controller";
+import verifyEmailAvailabilityMiddleware from "../middlewares/verifyEmailAvailability.middleware";
+import verifyAuthTokenMiddleware from "../middlewares/verifyAuthToken.middleware";
 
 const router = Router();
 
-router.post("", createUserController);
+router.post("/login", userLoginController);
+router.post("", verifyEmailAvailabilityMiddleware, createUserController);
 router.get("", listUsersController);
-router.put("/:id", updateUserController);
-router.delete("/:id", deleteUserController);
+router.put("/:id", verifyAuthTokenMiddleware, updateUserController);
+router.delete("/:id", verifyAuthTokenMiddleware, deleteUserController);
 
 export default router;
